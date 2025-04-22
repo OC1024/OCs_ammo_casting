7e35e621a70c097ed1ae81b9e71ac8bfa497be3d
OC1024 <testudo_gruppe@yahoo.de>
2025-04-22 21:32:41 +0200

    fixed wrong path for require(generator)

--- data.lua
+++ data.lua
@@ -1,5 +1,5 @@
 require("__OCs_base_assets__.prototypes.utils.helper")
-require("__OCs_base_assets__.prototypes.utils.generator") -- import but not executed
+require("__OCs_base_assets__.prototypes.utils.casting-generator") -- import but not executed
 require("prototypes.category")
 require("prototypes.plating_variants") -- Import the variant table
 require("prototypes.grid_definitions") -- defines and maps categories(personal, vehicle) to equipment_grid
